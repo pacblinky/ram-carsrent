3cf0654c5b99d15451af759aff3acd54aca75b6f
BelalGalhom <113742499+BelalGalhom@users.noreply.github.com>
2025-11-09 17:14:32 +0200

    changed directory of firebase

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,8 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
-// We no longer need JsonResponse
-// use Illuminate\Http\JsonResponse; 
+use Illuminate\Http\JsonResponse; // <-- 1. ADD THIS IMPORT
 
 class ProfileController extends Controller
 {
