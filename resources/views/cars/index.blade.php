8c8e9360f10a02279154ca6c66d580e4ae2e5f94
pacblinky <40526207+pacblinky@users.noreply.github.com>
2025-11-13 15:23:32 -0800

    max price is open

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -202,7 +202,7 @@
                             </div>
                             <div class="flex flex-col flex-1 text-center">
                                 <label for="max-price" class="mb-1 text-sm text-gray-600 dark:text-gray-400">{{ __('cars_page.max_price') }}</label>
-                                <input type="number" name="max_price" id="max-price" min="0" max="{{ $maxPriceInDb }}" 
+                                <input type="number" name="max_price" id="max-price" min="0" 
                                        value="{{ request('max_price', $maxPriceInDb) }}" 
                                        class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                             </div>
