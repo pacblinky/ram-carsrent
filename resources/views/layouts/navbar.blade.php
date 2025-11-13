d2fe4695e1fc307116f4a226a2af1b36aa4dda7f
BelalGalhom <113742499+BelalGalhom@users.noreply.github.com>
2025-11-13 06:27:56 +0200

    added header logo

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,8 +2,9 @@
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
         <div class="flex items-center space-x-3 rtl:space-x-reverse">
-            <a href="{{ route('home') }}" class="flex items-center">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{{ __('navbar.brand') }}</span>
+            <a href="{{ route('home') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="{{ asset('images/header-logoo.png') }}" class="h-[60px] w-[60px] rounded-full object-cover" alt="Brand Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap neon-animated-brand">{{ __('navbar.brand') }}</span>
             </a>
 
             <div class="hidden md:flex items-center space-x-3 rtl:space-x-reverse gap-2">
@@ -15,7 +16,7 @@
                         </path>
                     </svg>
                     <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
+                        xmlns="http://www.w3.org/2all.css/2000/svg">
                         <path
                             d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
                             fill-rule="evenodd" clip-rule="evenodd"></path>
