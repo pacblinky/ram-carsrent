b8adba47138c56227195bc6c0bb606219d9dafad
pacblinky <40526207+pacblinky@users.noreply.github.com>
2025-12-19 02:14:30 +0200

    updated sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -6,20 +6,47 @@
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>
-    <url>
-        <loc>{{ route('about') }}</loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.8</priority>
-    </url>
+
     <url>
         <loc>{{ route('cars.index') }}</loc>
         <changefreq>daily</changefreq>
         <priority>0.9</priority>
     </url>
+
+    <url>
+        <loc>{{ route('about') }}</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+
     <url>
         <loc>{{ route('contact.index') }}</loc>
         <changefreq>yearly</changefreq>
         <priority>0.5</priority>
+    </url>
+
+    <url>
+        <loc>{{ route('login') }}</loc>
+        <changefreq>yearly</changefreq>
+        <priority>0.6</priority>
+    </url>
+
+    <url>
+        <loc>{{ route('register') }}</loc>
+        <changefreq>yearly</changefreq>
+        <priority>0.6</priority>
+    </url>
+
+    <url>
+        <loc>{{ route('terms') }}</loc>
+        <changefreq>yearly</changefreq>
+        <priority>0.3</priority>
+    </url>
+
+    <url>
+        <loc>{{ route('privacy') }}</loc>
+        <changefreq>yearly</changefreq>
+        <priority>0.3</priority>
     </url>
 
     @foreach ($cars as $car)
