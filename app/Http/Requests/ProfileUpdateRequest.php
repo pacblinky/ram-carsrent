83883804d64bcf94ccc4f4ab52ffd8566227899e
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2025-12-25 17:46:44 +0200

    Update ProfileUpdateRequest.php

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -20,7 +20,7 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
-            'phone_number' => ['nullable', 'string', 'phone:INTERNATIONAL'],
+            'full_phone' => ['nullable', 'string', 'phone:INTERNATIONAL'],
             'government_id' => ['nullable', 'string', 'max:255'],
         ];
     }
